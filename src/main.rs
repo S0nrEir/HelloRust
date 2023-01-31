d123716ca97fe8da1652d771a2ff93f95ec8bfaa
YangHaoChen <x73901@live.com>
2023-01-31 18:36:31 +0800

    组织模块->第二和第三章

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 mod mod_02_guessing_game;//第二章猜数游戏
 mod mod_03_variables_and_mutability;//第三章vasriables_and_mutability
 
-
-
 //也可以使用这种方式一同包含Ordering和io
 // use std::{cmp::Ordering,io};
 
@@ -11,6 +9,6 @@ fn main(){
 }
 
 fn main_enter() {
-    mod_02_guessing_game::enter();
-    mod_03_variables_and_mutability::enter();
+    // mod_02_guessing_game::enter();
+    // mod_03_variables_and_mutability::enter();
 }
