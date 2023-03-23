ca8051a0942f9d62cfa254b7850f888865a3f5ab
YangHaoChen <x73901@live.com>
2023-03-23 15:15:44 +0800

    更新注释

--- src/mod_15_03_drop.rs
+++ src/mod_15_03_drop.rs
@@ -6,7 +6,7 @@ pub fn enter(){
     // }
 
     let a = CustomSmartPointer{ data:String::from("my stuff")};
-    //显式调用drop可以提前释放一个pointer指向的内存，
+    //显式调用std::mem::drop可以提前释放一个pointer指向的内存，
     //不用担心因为失误而使用了一个已经被提前释放的变量，因为Rust会在编译的时候帮你检查出问题并且报错
     std::mem::drop(a);
     // a.to_string();
